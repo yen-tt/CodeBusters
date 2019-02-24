4d6ed3b68884858272de6d7fbffbe61c20a4429f
Yen Truong <yen1027@vt.edu>
2019-02-23 22:45:21 -0500

    completed the last few steps of the Direct Messages feature

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -78,6 +78,8 @@ public class MessageServlet extends HttpServlet {
     String user = userService.getCurrentUser().getEmail();
     String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
     String recipient = request.getParameter("recipient");
+    System.out.println("print recipient:");
+    System.out.println(recipient);
     
     Message message = new Message(user, text, recipient);
     datastore.storeMessage(message);
