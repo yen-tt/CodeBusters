78c9c7ca8821dd69e244806283788ff3df1a06ec
Edwin_Carvajal <EdwinCarvajal@Edwins-MacBook-Pro-2.local>
2019-04-10 11:38:16 -0400

    CloudSql Database created. Dependencies and properties created in .xml file and properties for sql created in config.properties

--- src/main/java/com/google/codeu/servlets/CloudSqlServlet.java
+++ src/main/java/com/google/codeu/servlets/CloudSqlServlet.java
@@ -1,65 +1,60 @@
-    /*
-     * Copyright 2016 Google Inc.
-     *
-     * Licensed under the Apache License, Version 2.0 (the "License");
-     * you may not use this file except in compliance with the License.
-     * You may obtain a copy of the License at
-     *
-     * http://www.apache.org/licenses/LICENSE-2.0
-     *
-     * Unless required by applicable law or agreed to in writing, software
-     * distributed under the License is distributed on an "AS IS" BASIS,
-     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     * See the License for the specific language governing permissions and
-     * limitations under the License.
-     */
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-    package com.example.appengine.cloudsql;
+package com.example.appengine.cloudsql;
 
-    import com.google.apphosting.api.ApiProxy;
-    import com.google.common.base.Stopwatch;
-
-    import java.io.IOException;
-
-    import java.io.PrintWriter;
-    import java.net.Inet4Address;
-    import java.net.Inet6Address;
-    import java.net.InetAddress;
-    import java.sql.Connection;
-    import java.sql.DriverManager;
-    import java.sql.PreparedStatement;
-    import java.sql.ResultSet;
-    import java.sql.SQLException;
-    import java.sql.Timestamp;
-    import java.util.Date;
-    import java.util.Map;
-    import java.util.concurrent.TimeUnit;
-
-    import javax.servlet.ServletException;
-    import javax.servlet.annotation.WebServlet;
-    import javax.servlet.http.HttpServlet;
-    import javax.servlet.http.HttpServletRequest;
-    import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 // [START gae_java8_mysql_app]
 @SuppressWarnings("serial")
 // With @WebServlet annotation the webapp/WEB-INF/web.xml is no longer required.
-@WebServlet(name = "CloudSQL",
-    description = "CloudSQL: Write timestamps of visitors to Cloud SQL",
-    urlPatterns = "/cloudsql")
+@WebServlet(
+  name = "CloudSQL",
+  description = "CloudSQL: Store tax forum information",
+  urlPatterns = "/cloudsql"
+)
 public class CloudSqlServlet extends HttpServlet {
   Connection conn;
 
   @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException,
-      ServletException {
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException, ServletException {
 
-    final String createTableSql = "CREATE TABLE IF NOT EXISTS visits ( "
-        + "visit_id SERIAL NOT NULL, ts timestamp NOT NULL, "
-        + "PRIMARY KEY (visit_id) );";
-    final String createVisitSql = "INSERT INTO visits (ts) VALUES (?);";
-    final String selectSql = "SELECT ts FROM visits ORDER BY ts DESC "
-        + "LIMIT 10;";
+    final String createProfileTableSql =
+        "CREATE TABLE IF NOT EXISTS PROFILE ( "
+            + "profileId SERIAL NOT NULL, first VARCHAR NOT NULL, "
+            + "first VARCHAR NOT NULL,  isCPA BIT not NULL, "
+            + "PRIMARY KEY (profileId) );";
+    final String createPostTableSql =
+        "CREATE TABLE IF NOT EXISTS POST ( "
+            + "postId SERIAL NOT NULL, memberId SERIAL NOT NULL, "
+            + "header VARCHAR(255),  pointTotal INTEGER, "
+            + " FOREIGN KEY ( memberId ) REFERENCES PROFILE.profileId, PRIMARY KEY (postId) );";
+    //    final String createVisitSql = "INSERT INTO visits (ts) VALUES (?);";
+    //    final String selectSql = "SELECT ts FROM visits ORDER BY ts DESC "
+    //        + "LIMIT 10;";
 
     String path = req.getRequestURI();
     if (path.startsWith("/favicon.ico")) {
@@ -69,24 +64,26 @@ public class CloudSqlServlet extends HttpServlet {
     PrintWriter out = resp.getWriter();
     resp.setContentType("text/plain");
 
-    Stopwatch stopwatch = Stopwatch.createStarted();
-    try (PreparedStatement statementCreateVisit = conn.prepareStatement(createVisitSql)) {
-      conn.createStatement().executeUpdate(createTableSql);
-      statementCreateVisit.setTimestamp(1, new Timestamp(new Date().getTime()));
-      statementCreateVisit.executeUpdate();
-
-      try (ResultSet rs = conn.prepareStatement(selectSql).executeQuery()) {
-        stopwatch.stop();
-        out.print("Last 10 visits:\n");
-        while (rs.next()) {
-          String timeStamp = rs.getString("ts");
-          out.println("Visited at time: " + timeStamp);
-        }
-      }
-    } catch (SQLException e) {
-      throw new ServletException("SQL error", e);
-    }
-    out.println("Query time (ms):" + stopwatch.elapsed(TimeUnit.MILLISECONDS));
+    conn.createStatement().executeUpdate(createProfileTableSql);
+    conn.createStatement().executeUpdate(createPostTableSql);
+    //    Stopwatch stopwatch = Stopwatch.createStarted();
+    //    try (PreparedStatement statementCreateVisit = conn.prepareStatement(createVisitSql)) {
+    //      conn.createStatement().executeUpdate(createProfileTableSql);
+    //      statementCreateVisit.setTimestamp(1, new Timestamp(new Date().getTime()));
+    //      statementCreateVisit.executeUpdate();
+    //
+    //      try (ResultSet rs = conn.prepareStatement(selectSql).executeQuery()) {
+    //        stopwatch.stop();
+    //        out.print("Last 10 visits:\n");
+    //        while (rs.next()) {
+    //          String timeStamp = rs.getString("ts");
+    //          out.println("Visited at time: " + timeStamp);
+    //        }
+    //      }
+    //    } catch (SQLException e) {
+    //      throw new ServletException("SQL error", e);
+    //    }
+    //    out.println("Query time (ms):" + stopwatch.elapsed(TimeUnit.MILLISECONDS));
   }
 
   @Override
@@ -101,4 +98,3 @@ public class CloudSqlServlet extends HttpServlet {
   }
 }
 // [END gae_java8_mysql_app]
-
