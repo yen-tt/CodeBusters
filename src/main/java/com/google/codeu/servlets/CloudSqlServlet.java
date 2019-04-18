025e5b0e9add37bb079b070b7181403fc2f73347
Edwin_Carvajal <EdwinCarvajal@Edwins-MacBook-Pro-2.local>
2019-04-18 18:56:41 -0400

    SQL statements created for put, get, and post requests. Issues from Github resolved, and further implementation created. Testing has not been done, only development. DO NOT MERGE - NEEDS TESTING.

--- src/main/java/com/google/codeu/servlets/CloudSqlServlet.java
+++ src/main/java/com/google/codeu/servlets/CloudSqlServlet.java
@@ -42,20 +42,6 @@ public class CloudSqlServlet extends HttpServlet {
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException, ServletException {
 
-    final String createProfileTableSql =
-        "CREATE TABLE IF NOT EXISTS PROFILE ( "
-            + "profileId SERIAL NOT NULL, first VARCHAR NOT NULL, "
-            + "first VARCHAR NOT NULL,  isCPA BIT not NULL, "
-            + "PRIMARY KEY (profileId) );";
-    final String createPostTableSql =
-        "CREATE TABLE IF NOT EXISTS POST ( "
-            + "postId SERIAL NOT NULL, memberId SERIAL NOT NULL, "
-            + "header VARCHAR(255),  pointTotal INTEGER, "
-            + " FOREIGN KEY ( memberId ) REFERENCES PROFILE.profileId, PRIMARY KEY (postId) );";
-    //    final String createVisitSql = "INSERT INTO visits (ts) VALUES (?);";
-    //    final String selectSql = "SELECT ts FROM visits ORDER BY ts DESC "
-    //        + "LIMIT 10;";
-
     String path = req.getRequestURI();
     if (path.startsWith("/favicon.ico")) {
       return; // ignore the request for favicon.ico
@@ -64,26 +50,104 @@ public class CloudSqlServlet extends HttpServlet {
     PrintWriter out = resp.getWriter();
     resp.setContentType("text/plain");
 
-    conn.createStatement().executeUpdate(createProfileTableSql);
-    conn.createStatement().executeUpdate(createPostTableSql);
-    //    Stopwatch stopwatch = Stopwatch.createStarted();
-    //    try (PreparedStatement statementCreateVisit = conn.prepareStatement(createVisitSql)) {
-    //      conn.createStatement().executeUpdate(createProfileTableSql);
-    //      statementCreateVisit.setTimestamp(1, new Timestamp(new Date().getTime()));
-    //      statementCreateVisit.executeUpdate();
-    //
-    //      try (ResultSet rs = conn.prepareStatement(selectSql).executeQuery()) {
-    //        stopwatch.stop();
-    //        out.print("Last 10 visits:\n");
-    //        while (rs.next()) {
-    //          String timeStamp = rs.getString("ts");
-    //          out.println("Visited at time: " + timeStamp);
-    //        }
-    //      }
-    //    } catch (SQLException e) {
-    //      throw new ServletException("SQL error", e);
-    //    }
-    //    out.println("Query time (ms):" + stopwatch.elapsed(TimeUnit.MILLISECONDS));
+    if (request.getParameter("getQuestion") != null) {
+      // Perform get Question.
+      final String selectQuestionSql = "SELECT postId "
+          + "FROM POST "
+          + "WHERE header = '" + req.body.header + "'";
+
+      try(ResultSet rs = conn.prepareStatement(selectQuestionSql).executeQuery()){
+          System.out.println("Query successfully completed");
+          String queryResult = rs.getString(1);
+          resp.body.queryResult = queryResult;
+
+      }catch(SQLException e){
+          throw new ServletException("SQL error", e);
+      }
+    }
+
+  }
+
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException, ServletException {
+
+    String path = req.getRequestURI();
+    if (path.startsWith("/favicon.ico")) {
+      return; // ignore the request for favicon.ico
+    }
+
+
+    if (request.getParameter("saveAnswer") != null) {
+      // Perform save Answer.
+      final String createAnswerSql =
+          "INSERT INTO ANSWERS (postId, answer) " +
+          "VALUES ( " +
+          req.body.postID + ", " +
+          req.body.answer + " )";
+
+      try(ResultSet rs = conn.prepareStatement(createAnswerSql)){
+        System.out.println("Query successfully completed");
+        conn.createStatement().executeUpdate(createAnswerSql);
+
+      }catch(SQLException e){
+        throw new ServletException("SQL error", e);
+      }
+
+    }
+    else if (request.getParameter("saveQuestion") != null) {
+      // Perform save Question.
+      final String createQuestionSql =
+          "INSERT INTO POST (memberId, header, pointTotal) " +
+              "VALUES ( " +
+              req.body.memberId + ", " +
+              req.body.header + ", " +
+              req.body.pointTotal + " )";
+
+      try(ResultSet rs = conn.prepareStatement(createQuestionSql)){
+        System.out.println("Query successfully compiled");
+        conn.createStatement().executeUpdate(createQuestionSql);
+
+      }catch(SQLException e){
+        throw new ServletException("SQL error", e);
+      }
+
+    }
+
+
+
+  }
+
+  @Override
+  public void doPut(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException, ServletException {
+
+    String path = req.getRequestURI();
+    if (path.startsWith("/favicon.ico")) {
+      return; // ignore the request for favicon.ico
+    }
+
+
+    if (request.getParameter("updatePoints") != null) {
+      // Perform save Answer.
+      final String updatePointsSql =
+          "UPDATE POST " +
+              "SET ( pointTotal = " +
+              req.body.pointTotal + " )" +
+              "WHERE postId = " +
+              req.body.postId;
+    }
+
+    try(ResultSet rs = conn.prepareStatement(updatePointsSql)){
+      System.out.println("Query successfully compiled");
+      conn.createStatement().executeUpdate(updatePointsSql);
+
+    }catch(SQLException e){
+      throw new ServletException("SQL error", e);
+    }
+
+
+
   }
 
   @Override
@@ -95,6 +159,32 @@ public class CloudSqlServlet extends HttpServlet {
     } catch (SQLException e) {
       throw new ServletException("Unable to connect to Cloud SQL", e);
     }
+
+    final String createProfileTableSql =
+        "CREATE TABLE IF NOT EXISTS PROFILE ( "
+            + "ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY CLUSTERED, "
+            + "profileId AS 'UID' + RIGHT('00000000' + CAST(ID AS VARCHAR(8)), 8) PERSISTED, "
+            + "first VARCHAR NOT NULL, "
+            + "last VARCHAR NOT NULL,  isCPA BIT not NULL, "
+            + "PRIMARY KEY (profileId) );";
+    final String createPostTableSql =
+        "CREATE TABLE IF NOT EXISTS POST ( "
+            + "ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY CLUSTERED,"
+            + "postId AS 'UID' + RIGHT('00000000' + CAST(ID AS VARCHAR(8)), 8) PERSISTED,"
+            + "memberId SERIAL NOT NULL, "
+            + "header VARCHAR,  pointTotal INTEGER, "
+            + " FOREIGN KEY ( memberId ) REFERENCES PROFILE.profileId, PRIMARY KEY (postId) );";
+    final String createAnswerTableSql =
+        "CREATE TABLE IF NOT EXISTS ANSWERS ( "
+            + "ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY CLUSTERED,"
+            + "answerId AS 'UID' + RIGHT('00000000' + CAST(ID AS VARCHAR(8)), 8) PERSISTED,"
+            + "postId SERIAL NOT NULL, "
+            + "answer VARCHAR, "
+            + " FOREIGN KEY ( postId ) REFERENCES POST.postId, PRIMARY KEY (answerId) );";
+
+    conn.createStatement().executeUpdate(createProfileTableSql);
+    conn.createStatement().executeUpdate(createPostTableSql);
+    conn.createStatement().executeUpdate(createAnswerTableSql);
   }
 }
 // [END gae_java8_mysql_app]
